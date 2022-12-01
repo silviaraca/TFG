d320d15133927cd067d2359e0406dffb055de18d
javife15 <60519230+javife15@users.noreply.github.com>
2022-12-01 12:45:58 +0100

    Recogida de tomates
    He arreglado el recoger tomates con la E y he creado prefabs de los objetos para poder orientarnos mejor y que se actualicen todos los objetos a la vez en caso de modificar uno de un mismo tipo

--- Assets/Scripts/TomatoCollect.cs
+++ Assets/Scripts/TomatoCollect.cs
@@ -7,19 +7,31 @@ public enum CollectableType{NONE, TOMATO}
 public class TomatoCollect : MonoBehaviour
 {
    public CollectableType type;
+   private bool recogible;
+   private Player player;
+
+   private void Start(){
+       type = CollectableType.TOMATO;
+   }
+
+   private void Update(){
+        if(recogible && Input.GetKeyDown(KeyCode.E))
+            if(player.inventory.Add(type))
+                Destroy(this.gameObject);
+   }
    
    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Player player = collision.GetComponent<Player>();
-        type = CollectableType.TOMATO;
+        player = collision.GetComponent<Player>();
+        if(collision.gameObject.name.Equals("Player"))
+            recogible = true;        
+    }
 
-        if(player)
-        {
-            if(player.inventory.Add(type))
-            {
-                Destroy(this.gameObject);
-            }
-        }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        player = collision.GetComponent<Player>();
+        if(collision.gameObject.name.Equals("Player"))
+            recogible = false;
     }
 }
 
