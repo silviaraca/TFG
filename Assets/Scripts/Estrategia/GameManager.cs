347a8cac2a72c5118afb963c5996ca3464ed7d86
javife15 <60519230+javife15@users.noreply.github.com>
2023-02-06 11:48:15 +0100

    Robar cartas

---
+++ Assets/Scripts/Estrategia/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+  public List<Carta> deck = new List<Carta>();
+  public Transform[] cardSlots;
+  public bool[] availableCardSlots;
+
+  public void DrawCard(){
+    if(deck.Count >= 1){
+        Carta randCard = deck[Random.Range(0,deck.Count)];
+
+        for(int i = 0; i < availableCardSlots.Length; i++){
+            if(availableCardSlots[i] == true){
+                randCard.gameObject.SetActive(true);
+                randCard.transform.position = cardSlots[i].position;
+                availableCardSlots[i] = false;
+                deck.Remove(randCard);
+                return;
+            }
+        }
+    }
+  }
+}
