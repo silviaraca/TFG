df734ef815820e4219a56833eb32c1ec9c4d1488
javife15 <60519230+javife15@users.noreply.github.com>
2023-03-24 11:49:03 +0100

    healthBar emile te quiero

--- Assets/Scripts/Estrategia/HelthBar.cs
+++ Assets/Scripts/Estrategia/HelthBar.cs
@@ -5,17 +5,24 @@ using UnityEngine.UI;
 
 public class HelthBar : MonoBehaviour
 {
-    public GameObject bar;
+    public GameObject canv, bar;
     public Image healthBar, prevIm;
     public float maxHealth;
     private List<Image> barras = new List<Image>();
     private float posIni, dist, widthA;
-    private const float constScale = 68177E-5f;
     void Start()
     {
+        for(int i = 0; i < maxHealth;i++){
+            GameObject a = Instantiate(bar);
+            a.transform.SetParent(healthBar.transform, false);
+            barras.Add(a.gameObject.GetComponent<Image>());
+        }
+    }
+
+    /*
         RectTransform rt = healthBar.rectTransform;
-        widthA = (((rt.rect.width - 15 - (maxHealth+1)*2)) / maxHealth);
-        posIni = (healthBar.transform.position.x - ((rt.rect.width-15)*constScale/2)  + widthA*constScale/2 + 2);
+        widthA = (((rt.rect.width - (maxHealth+1)*2)) / maxHealth);
+        posIni = (healthBar.transform.position.x - (rt.rect.width/2)  + widthA/2 + 2);
         for(int i = 0; i < maxHealth; i++){
             GameObject a = Instantiate(bar);
             a.transform.SetParent(healthBar.transform, false);
@@ -23,10 +30,10 @@ public class HelthBar : MonoBehaviour
             if(i == 0)
                 a.transform.position = new Vector2(posIni, healthBar.transform.position.y);
             else{
-                a.transform.position = new Vector2(prevIm.transform.position.x + widthA*constScale + 120E-2f, healthBar.transform.position.y);
+                a.transform.position = new Vector2(prevIm.transform.position.x + widthA + 2, healthBar.transform.position.y);
             }
             prevIm =  a.gameObject.GetComponent<Image>();
             barras.Add(prevIm);
         }
-    }
+    */
 }
