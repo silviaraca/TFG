4716b69aa31374c2f196439001bd69007750789e
silviaraca <segido@ucm.es>
2023-08-02 12:25:28 +0200

    Garlic appear

--- Assets/Scripts/ScriptableObject/Garlic.cs
+++ Assets/Scripts/ScriptableObject/Garlic.cs
@@ -1,55 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Garlic : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
-    private bool isVisible = false;
-    private Player player;
     public TextMeshProUGUI textoE;
+    private SpriteRenderer spriteRenderer;
+    private Player player;
+    private bool dejable;
 
     void Start()
     {
-        // Obtener el componente SpriteRenderer del GameObject
         spriteRenderer = GetComponent<SpriteRenderer>();
-
-        // Ocultar el sprite al inicio del juego
         spriteRenderer.enabled = false;
         textoE.gameObject.SetActive(false);
+        isVisible = false;
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         player = collision.GetComponent<Player>();
         if(player){
             textoE.gameObject.SetActive(true);
-            spriteRenderer.enabled = true;
-            isVisible = true;
+            dejable = true;
         }                  
     }
 
-    void OnTriggerExit2D(Collider2D other)
+    void OnTriggerExit2D(Collider2D collision)
     {
         player = collision.GetComponent<Player>();
         if(collision.gameObject.name.Equals("Player")){
             textoE.gameObject.SetActive(false);
-            spriteRenderer.enabled = false;
-            isVisible = false;
-    
- 
+            dejable = false;
         } 
     }
 
     void Update()
     {
-        // Verificar si el sprite está visible y si el usuario presiona la tecla "E"
-        if (isVisible && Input.GetKeyDown(KeyCode.E))
+        if (dejable && Input.GetKeyDown(KeyCode.E))
         {
-            // Ocultar el sprite nuevamente
-            spriteRenderer.enabled = false;
-            isVisible = false;
-
-            // Aquí puedes agregar cualquier otro comportamiento que desees cuando el usuario presione "E".
-            // Por ejemplo, puedes incrementar una puntuación, reproducir un sonido, etc.
+            spriteRenderer.enabled = true;
         }
     }
 }
